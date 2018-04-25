c3da59765d1d559362724aa2d0bf27fcc5cf3e62
Christiebelle <cslester@outlook.com>
2018-04-25 13:10:33 +0100

    skeleton of header file created

--- src/containers/QuakeContainer.js
+++ src/containers/QuakeContainer.js
@@ -1,33 +1,53 @@
 import React, {Component} from 'react';
+import QuakeList from '../components/QuakeList';
 
 class QuakeContainer extends Component{
 
   constructor(props){
     super(props)
+    this.handleSelectChange = this.handleSelectChange.bind(this);
     this.state = {
       quakes: [],
       currentQuake: null
     }
   }
 
-  componentDidMount(url){
+  componentDidMount() {
+    this.loadEarthquakes(this.props.filters[0].url)
+  }
+
+  loadEarthquakes(url) {
     const request = new XMLHttpRequest();
     request.open('GET', url);
     request.onload = () => {
       if (request.status === 200) {
         const jsonString = request.responseText;
-        const quakesarray = JSON.parse(jsonString);
-        this.setState({quakes: quakesarray.features});
+        const quakesArray = JSON.parse(jsonString)
+        this.setState({quakes: quakesArray.features});
       }
     };
     request.send();
   }
 
-  render(){
-    return(
-      <h2>quake dropdown</h2>
+  handleSelectChange(event) {
+    this.loadEarthquakes(event.target.value);
+  }
+
+  render() {
+    return (
+      <div>
+        <Header
+          handleSelectChange={this.handleSelectChange}
+          filters={this.props.filters}
+        />
+        <QuakeLists
+          quakes={this.state.quakes}
+          url={this.props.filters[0].url}
+          handleSelectChange={this.handleSelectChange}
+        />
+      </div>
     )
   }
-};
+}
 
 export default QuakeContainer;
