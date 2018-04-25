9d67e25219218d36bfa4113462bd8f7d2f0841ca
Christiebelle <cslester@outlook.com>
2018-04-25 11:48:09 +0100

    filters added for quake magnitude

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const filters = [
+      {name: "All", url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"},
+      {name: "Sig", url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"},
+      {name: "Sml", url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson"},
+      {name: "Med", url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson"},
+      {name: "Lge", url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson"},
+
+    ]
+
+
     return (
       <div>
         <h1>Earthquake Tracker App</h1>
--- src/containers/QuakeContainer.js
+++ src/containers/QuakeContainer.js
@@ -4,6 +4,23 @@ class QuakeContainer extends Component{
 
   constructor(props){
     super(props)
+    this.state = {
+      quakes: [],
+      currentQuake: null
+    }
+  }
+
+  componentDidMount(url){
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onload = () => {
+      if (request.status === 200) {
+        const jsonString = request.responseText;
+        const quakesarray = JSON.parse(jsonString);
+        this.setState({quakes: quakesarray.features});
+      }
+    };
+    request.send();
   }
 
   render(){
