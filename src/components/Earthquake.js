a724693da6d96eae1c113cf5f4acc40f460951d2
Christiebelle <cslester@outlook.com>
2018-04-25 11:26:21 +0100

    Earthquake class basics set up

---
+++ src/components/Earthquake.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Earthquake extends Component{
+
+  constructor(){
+    super(props)
+  }
+
+  render(){
+    return(
+      <h3>more information on the earthquake - magnitude etc</h3>
+    )
+  }
+};
+
+export default Earthquake;
