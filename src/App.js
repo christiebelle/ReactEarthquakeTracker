eadab5392363c787ad8785007fd8f400f720c76b
Christiebelle <cslester@outlook.com>
2018-04-25 11:50:21 +0100

    refactors to the app file now that the size filters have been added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import QuakeContainer from './containers/QuakeContainer';
-import QuakeList from './components/QuakeList';
-import Earthquake from './components/Earthquake';
 import './App.css';
 
 class App extends Component {
@@ -12,17 +10,11 @@ class App extends Component {
       {name: "Sml", url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson"},
       {name: "Med", url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson"},
       {name: "Lge", url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson"},
-
     ]
 
 
     return (
-      <div>
-        <h1>Earthquake Tracker App</h1>
-        <QuakeContainer/>
-        <QuakeList/>
-        <Earthquake/>
-      </div>
+      <QuakeContainer filters={filters} />
     );
   }
 }
